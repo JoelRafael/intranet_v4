d88100497605b35540f5f0caf29be3f8ca5e9922
¨Joel <maria.brioso.joel@gmail.com>
2021-03-02 17:08:13 -0400

    Creando la reactividad

--- src/app/views/clientclub/clientclub.component.ts
+++ src/app/views/clientclub/clientclub.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-clientclub',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientclubComponent implements OnInit {
 
+validator:any
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+ 
 }
